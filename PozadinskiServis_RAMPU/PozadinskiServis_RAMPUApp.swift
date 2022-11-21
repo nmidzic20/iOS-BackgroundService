82a5e609948cbd3af12ab8d770d237f19dcfaee1
nmidzic20 <nmidzic20@foi.hr>
2022-11-21 17:41:19 +0100

    Initial Commit

---
+++ PozadinskiServis_RAMPU/PozadinskiServis_RAMPUApp.swift
@@ -0,0 +1,17 @@
+//
+//  PozadinskiServis_RAMPUApp.swift
+//  PozadinskiServis_RAMPU
+//
+//  Created by FOI on 21.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct PozadinskiServis_RAMPUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
